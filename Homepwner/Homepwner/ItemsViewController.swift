9dde15f5e59af5d13beabffdb235d4f582e3bb87
huangjiashu <huangjiashu@niubi.com>
2018-03-22 23:54:14 +0800

    编辑模式

--- Homepwner/Homepwner/ItemsViewController.swift
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -14,10 +14,30 @@ class ItemsViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        let statusBarHeight = UIApplication.shared.statusBarFrame.height
-//        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
-//        tableView.contentInset = insets
-//        tableView.scrollIndicatorInsets = insets
+        //        let statusBarHeight = UIApplication.shared.statusBarFrame.height
+        //        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        //        tableView.contentInset = insets
+        //        tableView.scrollIndicatorInsets = insets
+    }
+    
+    @IBAction func addNewItem(sender:AnyObject) {
+        
+        let item = itemsStore.createItem()
+        if let index = itemsStore.allItems.index(of: item) {
+            let indexPath = NSIndexPath(row: index, section: 0);
+            tableView.insertRows(at: [indexPath as IndexPath], with: .automatic)
+        }
+        
+    }
+    
+    @IBAction func toggleEditingMode(sender:AnyObject){
+        if isEditing {
+            sender.setTitle("Edit", for: .normal)
+            setEditing(false, animated: true)
+        }else {
+            sender.setTitle("Done", for: .normal)
+            setEditing(true, animated: true)
+        }
     }
     
     
@@ -32,4 +52,26 @@ class ItemsViewController: UITableViewController {
         cell.detailTextLabel?.text = "$\(item.valueInDollars)"
         return cell
     }
+    
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let item = itemsStore.allItems[indexPath.row]
+            let ac = UIAlertController(title: "Delete \(item.name) ?", message: "Are you sure you want to delete this item", preferredStyle: .actionSheet)
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            ac.addAction(cancelAction)
+            let deleteAction = UIAlertAction(title: "Delete", style: .destructive, handler: { (action) in
+                self.itemsStore.removeItem(item: item)
+                self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            })
+            ac.addAction(deleteAction)
+            present(ac, animated: true, completion: nil)
+        }
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        itemsStore.moveItemAtIndex(fromIndex: sourceIndexPath.row, toIndex: destinationIndexPath.row)
+    }
+    
 }
